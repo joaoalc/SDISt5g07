dfeecf19ff28cef39ba0ea990995337043e23e15
MrComboF10 <p.costa.azevedo@gmail.com>
2021-03-19 10:59:20 +0000

    change main paths and create Message

--- proj1/src/com/company/Main.java
+++ proj1/src/com/company/Main.java
@@ -10,10 +10,10 @@ public class Main {
         System.out.println("hello!");
         byte[] chunk = new byte[64000];
 	// write your code here
-        FileInputStream objReader = new FileInputStream(new File("files\\download.gif"));
+        FileInputStream objReader = new FileInputStream(new File("files/download.gif"));
 
         int numBytes = objReader.read(chunk, 0, 64000);
-        File outputFile = new File("files\\f.gif");
+        File outputFile = new File("files/f.gif");
         try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
             outputStream.write(chunk);
 
