1c2c318d47929811371a4572a75376b4dcd53310
MrComboF10 <p.costa.azevedo@gmail.com>
2021-03-21 12:30:37 +0000

    create some classes

--- proj1/src/com/company/Peer.java
+++ proj1/src/com/company/Peer.java
@@ -1,31 +1,34 @@
 package com.company;
 
-import java.io.IOException;
 import java.rmi.RemoteException;
 
 public class Peer implements IPeerRemote {
 
+    private Channel MC, MDB, MDR;
+
+    public Peer(String MCAddress, int MCPort, String MDBAddress, int MDBPort, String MDRAddress, int MDRPort) {
+        this.MC = new Channel(MCAddress, MCPort);
+        this.MDB = new Channel(MDBAddress, MDBPort);
+        this.MDR = new Channel(MDRAddress, MDRPort);
+    }
+
     @Override
     public void backup(String path, int replication) throws RemoteException {
-
+        // TODO: implement this
     }
 
     @Override
     public void restore(String path) throws RemoteException {
-
+        // TODO: implement this
     }
 
     @Override
     public void delete(String path) throws RemoteException {
-
+        // TODO: implement this
     }
 
     @Override
     public void reclaim(int space) throws RemoteException {
-
-    }
-
-    public static void main(String[] args) throws IOException {
-
+        // TODO: implement this
     }
 }
