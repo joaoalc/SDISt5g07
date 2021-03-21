7e3ad5d7791cc5141cbf6332feaa7667c77d0c3b
MrComboF10 <p.costa.azevedo@gmail.com>
2021-03-21 15:02:01 +0000

    Implement main method in Peer

--- proj1/src/com/company/Peer.java
+++ proj1/src/com/company/Peer.java
@@ -1,6 +1,10 @@
 package com.company;
 
+import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class Peer implements IPeerRemote {
 
@@ -30,5 +34,39 @@ public class Peer implements IPeerRemote {
     @Override
     public void reclaim(int space) throws RemoteException {
         // TODO: implement this
+    }
+
+    public static void main(String[] args) {
+
+        if (args.length != 9) {
+            System.out.println("Usage: Peer <protocol_version> <peer_id> <acess_point> <mc_address> <mc_port> <mdb_address> <mdb_port> <mdr_address> <mdr_port>");
+            System.exit(-1);
+        }
+
+        String protocolVersion = args[0];
+        int peerID = Integer.parseInt(args[1]);
+        String acessPoint = args[2];
+
+        String MCAddress = args[3];
+        int MCPort = Integer.parseInt(args[4]);
+
+        String MDBAddress = args[5];
+        int MDBPort = Integer.parseInt(args[6]);
+
+        String MDRAddress = args[7];
+        int MDRPort = Integer.parseInt(args[8]);
+
+        try {
+            Peer peer = new Peer(MCAddress, MCPort, MDBAddress, MDBPort, MDRAddress, MDRPort);
+            IPeerRemote stub = (IPeerRemote) UnicastRemoteObject.exportObject(peer, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind(acessPoint, stub);
+
+            System.err.println("Peer ready");
+        } catch (RemoteException | AlreadyBoundException e) {
+            e.printStackTrace();
+        }
     }
 }
